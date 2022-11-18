bd2e2cdc46ca43e3a7087b5d66f6289fa90440cf
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2022-11-18 19:14:59 +0100

    Create 7-remove-first-and-last-character.py

---
+++ Python/7-remove-first-and-last-character.py
@@ -0,0 +1,27 @@
+# Remove First and Last Character
+# Difficulty: 8 kyu
+
+# Solution 1
+def remove_char(s):
+    return s[1:-1]
+
+# Solution 2 (less concise)
+def remove_char(s):
+    return s[1:len(s)-1]
+
+# Test
+
+import codewars_test as test
+from solution import remove_char
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(remove_char('eloquent'), 'loquen')
+        test.assert_equals(remove_char('country'), 'ountr')
+        test.assert_equals(remove_char('person'), 'erso')
+        test.assert_equals(remove_char('place'), 'lac')
+        test.assert_equals(remove_char('ok'), '')
+        test.assert_equals(remove_char('ooopsss'), 'oopss')
+    
