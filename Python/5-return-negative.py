93bd86cad03fefbf8685c3fa843e9ea3d531bc9f
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2022-11-18 19:14:52 +0100

    Create 5-return-negative.py

---
+++ Python/5-return-negative.py
@@ -0,0 +1,20 @@
+# Return negative
+# Difficulty: 8 kyu
+
+def make_negative( number ):
+    return -abs(number)
+
+
+# Test
+
+import codewars_test as test
+from solution import make_negative
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        for n, expected in ((42,-42), (-9,-9), (0,0)):
+            actual = make_negative(n)
+            message = f"For n = {n}, expected {expected} but got {actual}"
+            test.assert_equals(actual, expected, message)
