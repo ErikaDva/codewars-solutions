f4f01b0c9ab078524f3800dfba8215a0cb2a9fa0
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2022-11-19 11:45:48 +0100

    Create 9-MakeUpperCase.py

---
+++ Python/9-MakeUpperCase.py
@@ -0,0 +1,17 @@
+# MakeUpperCase
+# Difficulty: 8 kyu
+
+def make_upper_case(s):
+    return s.upper()
+
+
+# Test
+
+import codewars_test as test
+from solution import make_upper_case
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(make_upper_case("hello"), "HELLO")
