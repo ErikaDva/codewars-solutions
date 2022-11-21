d078faca58d8cdd0c1c086fd889d330c8ec4ca81
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2022-11-21 19:06:11 +0100

    Create 8kyu-string-repeat.py

---
+++ Python/8kyu-string-repeat.py
@@ -0,0 +1,19 @@
+# String repeat
+# Difficulty: 8 kyu
+
+def repeat_str(repeat, string):
+    return repeat * string
+
+
+# Test
+import codewars_test as test
+from solution import repeat_str
+
+@test.describe('Fixed tests')
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(repeat_str(4, 'a'), 'aaaa')
+        test.assert_equals(repeat_str(3, 'hello '), 'hello hello hello ')
+        test.assert_equals(repeat_str(2, 'abc'), 'abcabc')
+    
