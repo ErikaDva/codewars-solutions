b20e009d9a8bcfc7fe736ac9e988b26c98b2e135
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2022-12-05 16:24:39 +0100

    Create 8kyu-you-can't-code-under-pressure-#1.sh

---
+++ Shell/8kyu/8kyu-you-can't-code-under-pressure-#1.sh
@@ -0,0 +1,18 @@
+# You Can't Code Under Pressure #1
+# Language: Shell
+# Difficulty: 8 kyu
+
+# $1 will be an integer. Double it and print it.
+echo $(( $1 * 2 ))
+
+
+# Tests
+describe "tests" do
+  it "fixed tests" do
+    expect(run_shell args: ['2']).to eq('4')
+    expect(run_shell args: ['4']).to eq('8')
+    expect(run_shell args: ['-10']).to eq('-20')
+    expect(run_shell args: ['0']).to eq('0')
+    expect(run_shell args: ['100']).to eq('200')
+  end
+end
