6da247b0048da6e31966694cad400a742e1d3b6c
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2023-02-15 23:02:15 +0100

    Create generateREADME.r

---
+++ generateREADME.r
@@ -0,0 +1,11 @@
+## ---------------------------
+##
+## Script name: generateREADME.r
+##
+## Purpose of script: generates README file for each language with a list of completed katas
+##
+## Author: Erika Dvarionaite
+##
+## Date Created: 2023-01-XX
+##
+## ---------------------------
