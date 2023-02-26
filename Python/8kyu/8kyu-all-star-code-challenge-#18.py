2df5b4962f2f8b75bbd84c5ef42c37b7aad6b6a6
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2023-02-26 21:25:28 +0100

    Create 8kyu-all-star-code-challenge-#18.py

---
+++ Python/8kyu/8kyu-all-star-code-challenge-#18.py
@@ -0,0 +1,20 @@
+# All Star Code Challenge #18
+# Language: Python
+# Version: 3.10
+# Difficulty: 8 kyu
+
+def str_count(strng, letter):
+    return 0 if len(strng) == 0 or len(letter) == 0 else strng.count(letter)
+
+
+# Tests
+@test.describe('Should return the correct character count')
+def fixed():
+    @test.it("")
+    def f():
+        test.assert_equals(str_count('hello', 'l'), 2)
+        test.assert_equals(str_count('hello', 'e'), 1)
+        test.assert_equals(str_count('codewars', 'c'), 1)
+        test.assert_equals(str_count('ggggg', 'g'), 5)
+        test.assert_equals(str_count('hello world', 'o'), 2)
+        test.assert_equals(str_count('', 'z'), 0)
